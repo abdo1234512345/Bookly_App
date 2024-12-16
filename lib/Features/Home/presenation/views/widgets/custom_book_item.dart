e88e39564b371dcb82491c5ae66bb3a8a9f62336
abdo1234512345 <41443491+abdo1234512345@users.noreply.github.com>
2024-12-16 18:53:21 +0200

    change

--- lib/Features/Home/presenation/views/widgets/custom_book_item.dart
+++ lib/Features/Home/presenation/views/widgets/custom_book_item.dart
@@ -3,23 +3,51 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
-class CustomBookImage extends StatelessWidget {
-  const CustomBookImage({super.key, required this.imageUrl});
+class CustomBookImage extends StatefulWidget {
+  const CustomBookImage({Key? key, required this.imageUrl}) : super(key: key);
   final String imageUrl;
+
+  @override
+  State<CustomBookImage> createState() => _CustomBookImageState();
+}
+
+class _CustomBookImageState extends State<CustomBookImage> {
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _simulateImageLoading();
+  }
+
+  Future<void> _simulateImageLoading() async {
+    // محاكاة تأخير تحميل الصورة لمدة 2 ثانية
+    await Future.delayed(const Duration(seconds: 2));
+    setState(() {
+      isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Skeletonizer(
-      child: AspectRatio(
-        aspectRatio: 2.7 / 4,
-        child: GestureDetector(
-          onTap: () {
-            GoRouter.of(context).push(AppRouter.KBookDetailsView);
-          },
+    return AspectRatio(
+      aspectRatio: 2.7 / 4,
+      child: GestureDetector(
+        onTap: () {
+          GoRouter.of(context).push(AppRouter.KBookDetailsView);
+        },
+        child: Skeletonizer(
+          enabled: isLoading, // عرض الهيكل العظمي أثناء التحميل
           child: Container(
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(16),
-              image: DecorationImage(
-                  fit: BoxFit.fill, image: NetworkImage(imageUrl)),
+              color: isLoading ? Colors.grey[300] : null, // لون الهيكل العظمي
+              image: isLoading
+                  ? null
+                  : DecorationImage(
+                      fit: BoxFit.fill,
+                      image: NetworkImage(widget.imageUrl),
+                    ),
             ),
           ),
         ),
