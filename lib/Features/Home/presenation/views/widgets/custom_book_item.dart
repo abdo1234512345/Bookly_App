a38225acf95db28e2a971a7f771e7afa6ee5c38e
abdo1234512345 <41443491+abdo1234512345@users.noreply.github.com>
2024-12-16 17:06:14 +0200

    add Skeletonizer for item

--- lib/Features/Home/presenation/views/widgets/custom_book_item.dart
+++ lib/Features/Home/presenation/views/widgets/custom_book_item.dart
@@ -1,23 +1,26 @@
 import 'package:bookly_app/Core/utils/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class CustomBookImage extends StatelessWidget {
   const CustomBookImage({super.key, required this.imageUrl});
   final String imageUrl;
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.7 / 4,
-      child: GestureDetector(
-        onTap: () {
-          GoRouter.of(context).push(AppRouter.KBookDetailsView);
-        },
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            image: DecorationImage(
-                fit: BoxFit.fill, image: NetworkImage(imageUrl)),
+    return Skeletonizer(
+      child: AspectRatio(
+        aspectRatio: 2.7 / 4,
+        child: GestureDetector(
+          onTap: () {
+            GoRouter.of(context).push(AppRouter.KBookDetailsView);
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              image: DecorationImage(
+                  fit: BoxFit.fill, image: NetworkImage(imageUrl)),
+            ),
           ),
         ),
       ),
